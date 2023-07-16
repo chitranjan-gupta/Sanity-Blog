d71cccabe70499ebb7c17e81936b95ac92b6106d
Chitranjan Gupta <chitranjankumargupta123@gmail.com>
2023-07-16 10:34:13 +0530

    Added support for self hosting

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,6 +1,9 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
+  project: {
+    basePath: '/studio'
+  },
   api: {
     projectId: 'ndix7yog',
     dataset: 'production'
