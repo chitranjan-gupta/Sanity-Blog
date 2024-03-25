411d4ce790ac1ee3eb0c374bec635ff3bd886578
Chitranjan Gupta <chitranjankumargupta123@gmail.com>
2024-03-25 22:12:01 +0530

    feat: Moved environment variables to .env

--- sanity.config.js
+++ sanity.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'Blog',
 
-  projectId: 'ndix7yog',
-  dataset: 'production',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_DATASET,
   server: {
     hostname: 'localhost',
     port: 3333,
